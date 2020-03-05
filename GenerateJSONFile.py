8a2d417be940fdc26548465cf37875ed79169de0
ricardo.morales <ricardo.morales@globant.com>
2020-03-04 23:34:26 -0600

    compress png files to jpg files for webapp and add jsonfilegenerator

---
+++ GenerateJSONFile.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+path = "./images/"
+repoimgpath="https://raw.githubusercontent.com/teabreaktime/pictogramas/master/images/"
+
+categories=os.listdir(path)
+pictograms={}
+for c in categories:
+    pictograms[c]=list(map(lambda v: repoimgpath+c+"/"+v,sorted(os.listdir(os.path.join(path,c)))))
+
+with open('pictogramas.json', 'w') as outfile:
+    json.dump(pictograms, outfile)
+    
